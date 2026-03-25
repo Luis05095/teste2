773646f727f9863f5d9e43344846b04aa523a392
Aluno <Aluno@fatecdiadema.com>
2026-03-25 09:20:24 -0300

    atualizei o arquivo

--- src/soma.js
+++ src/soma.js
@@ -6,3 +6,23 @@ function soma(a, b) {
 }
 
 module.exports = soma;
+
+function soma(a, b) {
+ let resultado = a + b;
+ let x = 10; // variável inútil
+ return resultado;
+}
+
+function soma(a, b) {
+ return a + b;
+}
+
+function soma2(a, b) {
+ return a + b;
+}
+
+function soma(a, b) {
+eval("console.log('teste')"); // linha problemática
+return a + b;
+}
+module.exports = soma;
